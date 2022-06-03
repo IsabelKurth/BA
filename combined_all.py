4d56c5092a3de93a62b3e16f2392460aad30ac15
IsabelKurth <isabel.kurth@t-online.de>
2022-06-03 17:17:52 +0200

    update

--- combined_all.py
+++ combined_all.py
@@ -2,25 +2,34 @@ import pandas as pd
 
 # load satellite data
 satellite_data_all = pd.read_pickle('satellite_all.pkl')
+satellite_data_night = pd.read_pickle('satellite_all_night.pkl')
 # load street data
 street_data_all = pd.read_pickle('street_all.pkl')
 
 # load water index data
-water_data = pd.read_pickle('water.pkl')
+water_data = pd.read_csv('dhs_final_labels.csv')
 
 # combine data
 combined_satellite = pd.merge(satellite_data_all, water_data, how="left", on=['DHSID_EA'])
+combined_satellite_night = pd.merge(satellite_data_night, water_data, how="left", on=['DHSID_EA'])
 combined_street = pd.merge(street_data_all, water_data, how="left", on=['DHSID_EA'])
 
 # get id, red, green, blue, water index 
 just_need_satellite = combined_satellite[['DHSID_EA', 'red', 'green', 'blue', 'water_index', 'year', 'path']]
+just_need_satellite_night = combined_satellite_night[['DHSID_EA', 'mean_nl', 'water_index', 'year', 'path']]
 just_need_street = combined_street[['DHSID_EA', 'red', 'green', 'blue', 'water_index']]
 just_need_satellite = just_need_satellite.dropna(subset=['water_index'])
+just_need_satellite_night = just_need_satellite_night.dropna(subset=['water_index'])
 just_need_street = just_need_street.dropna(subset=['water_index'])
 
-print(just_need_satellite.keys())
-
 just_need_satellite.to_pickle("C:\\Users\\isabe\\Documents\\BA\\BA\\finish_satellite.pkl")
+just_need_satellite_night.to_pickle("C:\\Users\\isabe\\Documents\\BA\\BA\\finish_satellite_night.pkl")
 just_need_street.to_pickle("C:\\Users\\isabe\\Documents\\BA\\BA\\finish_street.pkl")
 
+water_na_free = water_data.dropna(subset=['water_index'])
+dropped_rows = water_data[~water_data.index.isin(water_na_free.index)]
+print(dropped_rows.head())
+print(dropped_rows.groupby('cname').sum())
+print(water_data.shape)
+print(water_na_free.shape)
 
--- linear_regression_all.py
+++ linear_regression_all.py
@@ -13,23 +13,30 @@ import seaborn as sns
 # load data 
 data_satellite_all = pd.read_pickle('finish_satellite.pkl')
 data_street_all = pd.read_pickle('finish_street.pkl')
+data_satellite_night = pd.read_pickle('finish_satellite_night.pkl')
 data_street_all = data_street_all.iloc[1:,:]
 data_satellite_all = data_satellite_all.iloc[1:,:]
 print(data_street_all.shape)
 print(data_satellite_all.shape)
+print(data_satellite_night.shape)
 print(data_street_all.head())
 print(data_satellite_all.head())
+print(data_satellite_night.head())
 
 data_satellite_all_RGB = data_satellite_all.iloc[:,1:4]
 data_street_all_RGB = data_street_all.iloc[:,1:4]
 
 X_satellite_all = data_satellite_all_RGB.values
 Y_satellite_all = data_satellite_all['water_index'].values
+X_satellite_night = data_satellite_night['mean_nl']
+Y_satellite_night = data_satellite_night['water_index'].values
 X_street_all = data_street_all_RGB.values
 Y_street_all = data_street_all['water_index'].values
 
 x_train_satellite_all, x_test_satellite_all, y_train_satellite_all, y_test_satellite_all = train_test_split(X_satellite_all, Y_satellite_all, test_size = 0.2, train_size=0.8)
 x_train_street_all, x_test_street_all, y_train_street_all, y_test_street_all = train_test_split(X_street_all, Y_street_all, test_size = 0.2, train_size=0.8)
+x_train_satellite_night, x_test_satellite_night, y_train_satellite_night, y_test_satellite_night = train_test_split(X_satellite_night, Y_satellite_night, test_size = 0.2, train_size=0.8)
+
 
 model_satellite = LinearRegression()
 model_satellite.fit(x_train_satellite_all, y_train_satellite_all)
@@ -38,6 +45,20 @@ pred_satellite = model_satellite.predict(x_test_satellite_all)
 plt.scatter(y_test_satellite_all, pred_satellite)
 plt.show()
 sns.regplot(x=y_test_satellite_all, y=pred_satellite)
+plt.show()
+
+print(x_train_satellite_all.shape)
+print(x_train_satellite_night.shape)
+print(y_train_satellite_all.shape)
+print(y_train_satellite_night.shape)
+
+x_train_satellite_night = x_train_satellite_night.array
+model_satellite_night = LinearRegression()
+model_satellite_night.fit(x_train_satellite_night, y_train_satellite_night)
+pred_satellite_night = model_satellite_night.predict(x_test_satellite_night)
+plt.scatter(y_test_satellite_night, pred_satellite_night)
+plt.show()
+sns.regplot(x=y_test_satellite_night, y= pred_satellite_night)
 plt.show()
 
 
